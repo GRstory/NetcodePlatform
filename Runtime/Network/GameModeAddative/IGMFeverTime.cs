4225406c3cba5a168d5ca1d37f32267025d79802
GRstory <aratino5165@naver.com>
2025-10-10 15:35:18 +0900

    버그픽스

--- Runtime/Network/GameModeAddative/IGMFeverTime.cs
+++ Runtime/Network/GameModeAddative/IGMFeverTime.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class IGMFeverTime : MonoBehaviour
+public interface IGMFeverTime
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void AddFeverGuage(float value);
 }
